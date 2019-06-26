69dc66514dc19b64c63912c627b473c1ef2d2dcf
Saeed Afshari (BLAZE) <s_a@live.com>
2019-06-26 22:35:54 +0200

    App Template

---
+++ ItemTemplates/App/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using RedCorners.Forms;
+using System.Threading.Tasks;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace $rootnamespace$
+{
+    public partial class $safeitemname$ : Application2
+    {
+        public override void InitializeSystems()
+        {
+            InitializeComponent();
+            base.InitializeSystems();
+        }
+
+        public override Page GetFirstPage() => 
+            new Views.MainPage();
+    }
+}
---
+++ ItemTemplates/BindableModel/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using RedCorners.Forms;
+using RedCorners.Models;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : BindableModel
+    {
+        public $safeitemname$()
+        {
+            Status = TaskStatuses.Success;
+        }
+    }
+}
